e80435732eac6fd4219245c99f478b5b6ff553ad
PaoMultiverse <baot21278@gmail.com>
2024-04-25 02:18:04 +0700

    HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Footer from './components/Footer'
+import { Route,Routes } from 'react-router-dom'
 
 export default function App() {
   return (
     <>
       <Navbar/>
-      <Home/>
+      <Routes>
+        <Route path='/GrabFood/' element={<Home/>}/>
+      </Routes>
       <Footer/>
     </>
   )
